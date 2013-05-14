d9098838bce5704b0108ac97c55cfc0f63ac0a96
rob murray <robmurray17@gmail.com>
2013-05-14 21:06:42 +0100

    Tweaked motd

--- puppet/manifests/config.pp
+++ puppet/manifests/config.pp
@@ -10,7 +10,7 @@ class config {
         ensure  => file,
         path    => '/etc/motd',
         mode    => 0644,
-        content => "Host: ${hostname}, OS: ${operatingsystem}, IP: ${ipaddress}\n vagrant-javadev-box | https://github.com/rob-murray/vagrant-javadev-box\n",
+        content => "Host: ${hostname}, OS: ${operatingsystem}, IP: ${ipaddress}\nVagrant config: vagrant-javadev-box | https://github.com/rob-murray/vagrant-javadev-box\n",
     }
 }
 
