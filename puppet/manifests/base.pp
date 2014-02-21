4bb818bad34b1a2c0f9c6ad8c0d256491f073e31
Rob Murray <robmurray17@gmail.com>
2014-02-21 20:02:32 +0000

    Update Postgres, Maven and remove config

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -6,8 +6,6 @@
 # https://github.com/rob-murray/vagrant-javadev-box
 #
 
-# Include linux box config
-include config
 
 # Install latest jdk
 class { 'java':
@@ -15,22 +13,32 @@ class { 'java':
   version      => 'latest',
 }
 
-# Install and configure Postgresql
+# Postgresql general config
+class { 'postgresql':
+  version => '9.2',
+  manage_package_repo => true,
+  charset => 'UTF8',
+  locale  => 'en_US.UTF-8'
+}
+
+# Postgresql server config
 class { 'postgresql::server':
   config_hash => {
     'ip_mask_deny_postgres_user' => '0.0.0.0/32',
     'ip_mask_allow_all_users'    => '0.0.0.0/0',
     'listen_addresses'           => '*',
-    'ipv4acls'                   => ['hostssl all johndoe 10.200.17.0/24 cert'],
     'manage_redhat_firewall'     => true,
     'postgres_password'          => 'letmein',
   },
 }
 
+# Postgresql database config
+# no op
+
 # Install Maven to the vagrant users home dir and shell
 maven::setup { "maven":
   ensure        => 'present',
-  source        => 'apache-maven-3.0.5-bin.tar.gz',
+  source        => 'apache-maven-3.1.1-bin.tar.gz',
   deploymentdir => '/home/vagrant/apache-maven',
   user          => 'vagrant',
   pathfile      => '/home/vagrant/.bashrc'
