1e4ecf204b354b0d68cffe0b765f56ad93e6b364
Rob Murray <robmurray17@gmail.com>
2015-04-09 08:40:37 +0100

    Auto download Maven bin.tar.gz
    Add wget puppet submodule
    
    Sync the puppet/modules dir so we can download maven bin to it

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -5,7 +5,7 @@
 # vagrant-javadev-box
 # https://github.com/rob-murray/vagrant-javadev-box
 #
-
+include wget
 
 # Install latest jdk
 class { 'java':
@@ -36,10 +36,24 @@ class { 'postgresql::server':
 # If necessary, config here to build application specific database
 
 # Install Maven to the vagrant users home dir and shell
+file { [ "/puppet/maven/files" ]:
+  ensure => "directory",
+}
+
+wget::fetch { "download-maven-bin":
+  source      => "http://mirrors.ukfast.co.uk/sites/ftp.apache.org/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz",
+  destination => "/puppet/maven/files/apache-maven-3.2.5-bin.tar.gz",
+  verbose     => true,
+  require     => File[ "/puppet/maven/files" ]
+}
+
+include maven
+
 maven::setup { "maven":
   ensure        => 'present',
-  source        => 'apache-maven-3.2.3-bin.tar.gz',
+  source        => 'apache-maven-3.2.5-bin.tar.gz',
   deploymentdir => '/home/vagrant/apache-maven',
   user          => 'vagrant',
-  pathfile      => '/home/vagrant/.bashrc'
+  pathfile      => '/home/vagrant/.bashrc',
+  require => Wget::Fetch['download-maven-bin']
 }
